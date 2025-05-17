3bf6abf44803192a5fd763686d2433ec1c4b4a6a
Alex Selyutin <selutin.alexandr90@gmail.com>
2025-05-18 02:10:19 +0300

    comparison

---
+++ src/topics/comparison.kt
@@ -0,0 +1,14 @@
+package topics
+
+fun main() {
+
+    val userAge = 66
+    val comparisonResult: Boolean = (userAge >= AGE_OF_MAJORITY) && !(userAge >= RETIREMENT_AGE)
+    val result = userAge in AGE_OF_MAJORITY..RETIREMENT_AGE
+    println("Result is: $comparisonResult")
+    println(result)
+
+}
+
+const val AGE_OF_MAJORITY = 18
+const val RETIREMENT_AGE = 65
